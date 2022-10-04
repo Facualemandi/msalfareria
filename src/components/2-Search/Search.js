f9f35b3b7b2902de3cb845cb7bc774aa616b4824
Facundo Alemandi <facuinstituto10@gmail.com>
2022-10-04 11:44:37 -0300

    Arrreglando position fized de Hamburger Menu

--- src/components/2-Search/Search.js
+++ src/components/2-Search/Search.js
@@ -1,26 +1,30 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
-
-const Input= styled.input`
-padding: 15px;
-@media (max-width: 600px){
+const SectionInput = styled.section`
+  padding-top: 70px;
+`;
+const Input = styled.input`
+  padding: 15px;
+  @media (max-width: 600px) {
     width: 90vw;
     display: flex;
     margin: auto;
     border: none;
     border: 1px solid rgba(0, 0, 0, 0.13);
     border-radius: 10px;
-    box-shadow: 2px 2px 2px 0 rgba(0, 0, 0, 0.080);
+    box-shadow: 2px 2px 2px 0 rgba(0, 0, 0, 0.08);
     outline-color: lightblue;
-}
-`
+  }
+`;
 const Search = () => {
   return (
     <>
-     <Input placeholder='Ingresa el producto' />
+      <SectionInput>
+        <Input placeholder="Ingresa el producto" />
+      </SectionInput>
     </>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
